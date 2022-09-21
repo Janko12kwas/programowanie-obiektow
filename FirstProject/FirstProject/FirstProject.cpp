697c881c2c6dbbcdd453b68c50cc25488b7ac7c0
kacpergoluchowski <kgoluchowski112@gmail.com>
2022-09-21 09:54:20 +0200

    es

--- FirstProject/FirstProject/FirstProject.cpp
+++ FirstProject/FirstProject/FirstProject.cpp
@@ -1,6 +1,47 @@
-﻿#include <iostream>
+﻿// FirstProject.cpp : This file contains the 'main' function.
+//  Program execution begins and ends there.
+
+
+#include <iostream>
 
 int main()
 {
-	std::cout << "Hello Word!\n";
+    /*
+    Instrukcja do wyświetlania informacji na standardowy strumień wyjścia.
+    Domyślnie, jesli nie zostało to zmienione, jest to konsola w której aplikacja
+    została uruchomiona.
+    Aby wyswietlić na konsoli nalezy użyć std::cout.
+    Na konosle wywyłamy za pomoca operatora <<.
+    Po nim musi znajdować się dana do wysłania.
+    Instrukcję kończymy znakiem średnik.
+    "Hello World!\n" - jest to stała o wartości tekstowej.
+    Stałe tekstowe są ograniczone znakami cudzysłowu.
+    * ' (apostrof) - jesli jest jeden znak
+    * " (cudzysłów) - jesli jest zero lub więcej znaków
+    W stałych tekstowych moga być wstawiane znaki specjalne. Takie znaki
+    zaczynają się od znaku \ (backslash) po którym może się znajdować:
+    * jedna litera alfabetu angielskiego
+    * trzy cyfry dające liczbę ósemkową
+    Przykładowo:
+    * \n - symbol oznaczający nową linię.
+    * \t - symbol oznaczający znak tabulatora
+    * \\ - symbol oznaczający znak \
+    * \' - symbol oznaczający znak '
+    * \" - symbol oznaczający znak "
+    * \141 - symbol ósemkowy oznaczajacy zgodnie z tablicą kodów ASCII znak młaej literki a
+    */
+
+    //github.com/IronLittleFox
+
+    std::cout << "Hel\nlo World!\n";
+    std::cout << "Hello World!\n";
+
+    std::cout << "Predkosc to km\\h\n";
+
+    std::cout << "Tabulator\ttabulator\n";
+
+    std::cout << 125.5 << " to jest jd " << 16 << "\n";
+
+    std::cout << 12635355 * 34;
 }
+
