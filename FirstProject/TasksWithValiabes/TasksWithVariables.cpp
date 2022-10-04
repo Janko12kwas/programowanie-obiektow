13951e8dd6464612c9919de3d5f222bad707b596
kacpergoluchowski <kgoluchowski112@gmail.com>
2022-10-04 09:04:50 +0200

    eszzz

--- FirstProject/TasksWithValiabes/TasksWithVariables.cpp
+++ FirstProject/TasksWithValiabes/TasksWithVariables.cpp
@@ -1,10 +1,50 @@
 ﻿#include <iostream>
 
+
+
+
+/*
+Napisz funkcje ktora pobierze dwie liczby oraz wykona podstawowa operacje arytmetyczne
+*/
+
+
+void task1()
+{
+
+    int firstNumber, secondNumber;
+
+    std::cout << "Podaj pierwsza liczbe:\n";
+    std::cin >> firstNumber;
+
+    std::cout << "Podaj druga liczbe:\n";
+    std::cin >> secondNumber;
+
+
+    int sum, difference, product, quotient, rest;
+
+    sum = firstNumber + secondNumber;
+    difference = firstNumber - secondNumber;
+    product = firstNumber * secondNumber;
+    quotient = firstNumber / secondNumber;
+    rest = firstNumber % secondNumber;
+
+    std::cout << "Suma: " << sum << "\n";
+    std::cout << "Roznica: " << difference << "\n";
+    std::cout << "Iloczyn: " << product << "\n";
+    std::cout << "Iloraz: " << quotient << "\n";
+    std::cout << "Reszta z dzielenia: " << rest << "\n";
+    
+    
+}
+
+
+
+
+
+
 int main()
 {
-    std::cout << "Hello World!\n";
+    task1();
 
 
-
-    
 }
