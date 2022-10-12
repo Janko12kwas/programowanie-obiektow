44212a97baa23189d41752ae52f2e5f08553b94e
MikolajKos <niko.kosiorek@gmail.com>
2022-10-12 09:50:33 +0200

    ewfrfwsef

---
+++ FirstProject/Decision instruction/Decision instruction.cpp
@@ -0,0 +1,38 @@
+﻿
+
+#include <iostream>
+
+/*
+Napisz funkcje, ktora wczyta dwie liczby i wykona dzielenie na nich
+*/
+
+
+void task1()
+{
+    int firstNumber, secondNumber;
+
+    std::cout << "Podaj pierwsza liczbe:\n";
+    std::cin >> firstNumber;
+
+    std::cout << "Podaj druga liczbe:\n";
+    std::cin >> secondNumber;
+
+
+    int quotient;
+
+   
+    quotient = firstNumber / secondNumber;
+    
+
+   
+    std::cout << "Iloraz: " << quotient << "\n";
+    
+
+}
+
+
+int main()
+{
+	task1();
+}
+
