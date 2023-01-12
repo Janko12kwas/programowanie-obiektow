315f190149cd421d7860bea11b88c41da45df566
MikolajKos <niko.kosiorek@gmail.com>
2023-01-12 15:33:41 +0100

    JD

--- FirstProject/Decision instruction/Decision instruction.cpp
+++ FirstProject/Decision instruction/Decision instruction.cpp
@@ -96,16 +96,153 @@ void task3()
         std::cout << "liczba nie jest w zakresie\n";
     }
 
-
+    
 }
 
+
+/*
+Napisz liczbe która wczyta liczbe od uzytkownika i wyswietli informacje czy jest ona parzysta czy nie.
+*/
+
+void task4()
+{
+    
+}
+
+void task5()
+{
+    int bok;
+
+    std::cout << "Podaj bok kwadratu:\n";
+    std::cin >> bok;
+    
+    int obwod;
+    int pole;
+
+    if (bok > 0)
+    {
+        obwod = bok * 4;
+        pole = bok * bok;
+        std::cout << "Obwod kwadratu: " << obwod << "\n";
+        std::cout << "Pole kwadratu: " << pole << "\n";
+    }
+    else
+        std::cout << "Podales bok ujemny.kys \n";
+}
+
+
+/*void task7()
+{
+    int firstNumber, secondNumber, thirdNumber;
+
+    std::cout << "Podaj pierwsza liczbe do sprawdzenia:\n";
+    std::cin >> firstNumber;
+
+    std::cout << "Podaj drugą liczbe do sprawdzenia:\n";
+    std::cin >> secondNumber;
+
+    std::cout << "Podaj trzecia liczbe do sprawdzenia:\n";
+    std::cin >> thirdNumber;
+
+    if (firstNumber > secondNumber || firstNumber > thirdNumber )
+        std::cout << "Liczba najwieksza: " << firstNumber << "\n";
+    else
+    {
+          if(secondNumber > firstNumber)
+               std::cout << "Liczba najwieksza: " << secondNumber << "\n";
+    }
+    else
+    {
+        std::cout << "Liczba najwieksza: " << thirdNumber << "\n";
+    }
+}*/
+
+/*void task8()
+{
+    int firstNumber, secondNumber;
+
+    std::cout << "Podaj pierwsza liczbe do sprawdzenia:\n";
+    std::cin >> firstNumber;
+
+    std::cout << "Podaj druga liczbe do sprawdzenia:\n";
+    std::cin >> secondNumber;
+
+    if (secondNumber > firstNumber)
+        std::cout << "Kolejnosc rosnaca: " << secondNumber << " " << firstNumber;
+    else
+    {
+        std::cout << "Kolejnosc rosnaca: " << firstNumber << " " << secondNumber;
+    }
+}*/
+
+
+
+
+
+ void task10()
+{
+    int dayNumber;
+
+
+    std::cout << "Podaj numer dnia tygodnia do sprawedzenia:\n";
+    std::cin >> dayNumber;
+
+    switch (dayNumber)
+    {
+    case 1:
+        std::cout << "Poniedzialek\n";
+        break;
+    case 2:
+        std::cout << "Wtorek\n";
+        break;
+    case 3:
+        std::cout << "Sroda\n";
+        break;
+    case 4:
+        std::cout << "Czwartek\n";
+        break;
+    case 5:
+        std::cout << "Piatek\n";
+        break;
+    case 6:
+        std::cout << "Sobota\n";
+        break;
+    case 0:
+    case 7:
+        std::cout << "Niedziela\n";
+        break;
+    default:
+        std::cout << "Bledny dzien\n";
+    }
+}
+
+ void task12()
+{
+     int numberToCheck;
+
+     std::cout << "Podaj temperature do sprawdzenia:\n";
+     std::cin >> numberToCheck;
+
+     if (numberToCheck <= 0)
+         std::cout << "Bardzo zimno\n";
+     if(numberToCheck >= 1 || numberToCheck < 15)
+         std::cout << "zimno\n";
+
+}
 
 int main()
 {
 	//task1();
     //task2();
-    task3();
-
+    //task3();
+    //task4();
+    //task5();
+    //task6();
+    //task7();
+    //task8();
+    //task12();
+    //task10();
+    task12();
 }  
     
-
+  
---
+++ FirstProject/loopInstructions/loopInstructions.cpp
@@ -0,0 +1,202 @@
+﻿#include <iostream>
+
+void task1()
+{
+	int number;
+
+
+	do
+	{
+		std::cout << "Podaj liczbe dodatnia: \n";
+		std::cin >> number;
+	} while (number <= 0);
+	
+	std::cout << "Podales liczbe " << number << "\n";
+}
+
+/*void task2()
+{
+	int a, b, c;
+	a = 0;
+	std::cout << "Podaj liczbe" << "\n";
+	std::cin >> c;
+	do
+	{
+		a = a + 1;
+		std::cout << a << "\n";
+	} while (a < c);
+}*/
+
+/*void task3()
+{
+	int lowRange, highRange;
+	std::cout << "Podaj zakres dolny\n";
+	std::cin >> lowRange;
+	std::cout << "Podaj zakres gorny\n";
+	std::cin >> highRange;
+
+	int currentNumber = lowRange;
+
+	do
+	{
+		std::cout << currentNumber << " \n";
+		currentNumber++;
+	} while (currentNumber != highRange + 1);
+}*/
+
+void task4()
+{
+	int randomNumber;
+
+	srand(time(NULL));
+	
+	randomNumber = rand() % 10 + 1;
+		int numberFromUser;
+
+	do
+	{
+		std::cout << "Podaj liczbe: \n";
+		std::cin >> numberFromUser;
+	} while (numberFromUser != randomNumber);
+
+		std::cout << "Gratulaccje";
+} 
+
+/*void task5()
+{
+	int lowRange, highRange;
+	std::cout << "Podaj zakres dolny\n";
+	std::cin >> lowRange;
+	std::cout << "Podaj zakres gorny\n";
+	std::cin >> highRange;
+
+	int currentNumber = lowRange;
+
+	/*
+	{
+		std::cout << currentNumber << " \n";
+		currentNumber++;
+	} while (currentNumber <= highRange + 1);
+	*
+	
+	while (currentNumber <= highRange)
+	{
+		std::cout
+	}
+} */
+
+void task6()
+{
+	int firstNumber, secondNumber;
+	std::cout << "Podaj pierwsz¹ liczbê\n";
+	std::cin >> firstNumber;
+	std::cout << "Podaj drug¹ liczbê\n";
+	std::cin >> secondNumber;
+
+	int dzielnik;
+	if (firstNumber > secondNumber)
+		dzielnik = secondNumber;
+	else
+		dzielnik = firstNumber;
+	//algorytm NWD
+	while (firstNumber % dzielnik > 0 || secondNumber % dzielnik > 0)
+		dzielnik--;
+
+	std::cout << "NWD = " << dzielnik << "\n";
+
+	int a = firstNumber;
+	int b = secondNumber;
+
+	while (b != a)
+	{
+		int tmA = a;
+		a = b;
+		b = tmA % b;
+	}
+
+	std::cout << "NWD = " << dzielnik << "\n";
+	std::cout << "NWD = " << a << "\n";
+}
+
+/*  void task7()
+{
+	int numberFromUser;
+	std::cout << "Podaj liczbe:\n";
+	std::cin >> numberFromUser;
+
+	unsigned long long result = 1;
+
+	int i = 2;
+	/*while (i <= numberFromUser)
+	{
+		result = result * i;
+		i++;
+	}
+
+	std::cout << numberFromUser << "! = " << result << "\n";
+
+	for (int i = 2; i <= numberFromUser; i++)
+	{
+		result = result * i;
+	}
+
+	std::cout << numberFromUser << "! = " << result << "\n";
+*/
+
+void task8()
+{
+	std::string textFromUser;
+	std::cout << "Podaj wyraz\n";
+	std::cin >> textFromUser;
+
+
+	//textFromUser[0] = 'A';
+	std::cout << "U¿ytkownik poda³: " << textFromUser << "\n";
+	std::cout << "Napis sklada sie z " << textFromUser.length() << " znaków\n";
+	std::cout << "Pierwszy znak z napisu to " << textFromUser[0] << "\n";
+	std::cout << "Ostatni znak z napisu to " << textFromUser[textFromUser.length() - 1] << "\n";
+
+	int coutChar = 0;
+	for (int i = 0; i < textFromUser.length(); i++)
+	{
+		if (textFromUser[i] == 'a')
+			coutChar++;
+	}
+
+
+	std::cout << "Znaków 'a' jest " << coutChar << "\n";
+}
+
+void task9()
+{
+	std::string textFromUser;
+	std::cout << "Podaj wyraz\n";
+	std::cin >> textFromUser;
+
+	for (int i = 0; i < textFromUser.length(); i++)
+	{
+		if (textFromUser[i] >= 'A' && textFromUser[i] <= 'Z')
+			textFromUser[i] = textFromUser[i] + 32;
+		else if(textFromUser[i] >= 'a' && textFromUser[i] <= 'z')
+			textFromUser[i] = textFromUser[i] - 32;
+	}
+
+	std::cout << "Napis po zmianie: " << textFromUser << "\n";
+}
+
+
+
+int main()
+{
+	setlocale(LC_CTYPE, "polish");
+
+	//task1();
+	//task2();
+	//task3();
+	//task4();
+	//task5();
+	//task6();
+	//task7();
+	//task8();
+	task9();
+}
